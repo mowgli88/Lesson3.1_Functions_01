dcc8d73bb2f4801fa62167669b9f36ea966df5d7
Tim Allan <timallan@tims-imac.local>
2016-04-06 13:23:21 +0100

    Initial Commit

---
+++ Lesson3.1_Functions_01/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lesson3.1_Functions_01
+//
+//  Created by Tim Allan on 06/04/2016.
+//  Copyright © 2016 Sprink. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
